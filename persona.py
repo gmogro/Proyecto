f3d5483f66152969d4f3f51ab033b67bf74a1c97
gmogro <guillermomogro51@gmail.com>
2022-10-20 21:41:08 -0300

    Agregue el set y get de nombre

--- persona.py
+++ persona.py
@@ -2,7 +2,16 @@ class Persona:
     def __init__(self,nombre,apellido):
         self.__nombre = nombre
         self.__apellido = apellido
-    
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self,nombre):
+        self.__nombre = nombre
+
+
+
     
     
     
